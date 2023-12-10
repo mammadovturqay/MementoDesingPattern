ded7a7afd46c14b23efb108230250fe6a4773f93
= <=Turqay@mail.ru>
2023-12-10 22:06:06 +0400

    Add project files.

---
+++ MemmentoDesingP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+public interface IState
+{
+    void Handle();
+}
+
+public class OnState : IState
+{
+    public void Handle()
+    {
+        Console.WriteLine("Lights are on");
+    }
+}
+
+public class OffState : IState
+{
+    public void Handle()
+    {
+        Console.WriteLine("Lights are off");
+    }
+}
+
+public class Context
+{
+    private IState state;
+
+    public IState State
+    {
+        get { return state; }
+        set { state = value; }
+    }
+
+    public Context(IState state)
+    {
+        this.state = state;
+    }
+
+    public void Request()
+    {
+        state.Handle();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Context context = new Context(new OffState());
+        context.Request(); 
+
+        context.State = new OnState();
+        context.Request();
+    }
+}
